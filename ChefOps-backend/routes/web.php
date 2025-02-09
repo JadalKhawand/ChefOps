14d787d4c927f4c31327e0f7768dae5d1a391c08
Jad alKhawand <jkh42005@gmail.com>
2025-02-10 00:18:45 +0200

    added CRUD for menu

--- ChefOps-backend/routes/web.php
+++ ChefOps-backend/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/api/menus', [MenuController::class, 'index'])->name('menus.index');
+
+Route::post('/api/menus', [MenuController::class, 'store'])->name('menus.store');
+
+Route::get('/api/menus/{menu}', [MenuController::class, 'show'])->name('menus.show');
+
+Route::put('/api/menus/{menu}', [MenuController::class, 'update'])->name('menus.update');
+
+Route::apiResource('menus', MenuController::class);Route::apiResource('menus', MenuController::class);Route::delete('/api/menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
